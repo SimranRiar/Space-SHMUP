cbefca522268553e0cc11df5892726a74dd6d0ba
SimranRiar <simriar@gmail.com>
2018-07-03 16:21:24 -0400

    9 - Hero Destruction and Game Restarting

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -1,44 +1,91 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Hero : MonoBehaviour {
+public class Hero : MonoBehaviour
+{
 
-	static public Hero		S;
+    static public Hero S;
 
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+    public float gameRestartDelay = 2f;
 
-    [Header("Set in inspector")]
-	public float	speed = 30;
-	public float	rollMult = -45;
-	public float  	pitchMult=30;
+    [SerializeField]
+    public float _shieldLevel = 1;
 
-    [Header("Set dynamically")]
-	public float	shieldLevel=1;
-
+    private GameObject lastTriggerGo = null;
 
     private void Awake()
     {
         if (S == null)
         {
             S = this;
-        } else
+        }
+        else
         {
             Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
         }
     }
 
 
-	
-	// Update is called once per frame
-	void Update () {
-		float xAxis = Input.GetAxis("Horizontal");
-		float yAxis = Input.GetAxis("Vertical");
 
-		Vector3 pos = transform.position;
-		pos.x += xAxis * speed * Time.deltaTime;
-		pos.y += yAxis * speed * Time.deltaTime;
-		transform.position = pos;
-		
-		// rotate the ship to make it feel more dynamic
-		transform.rotation =Quaternion.Euler(yAxis*pitchMult, xAxis*rollMult,0);
-	}
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Transform rootT = other.gameObject.transform.root;
+
+        GameObject go = rootT.gameObject;
+
+        //print("Triggered: " + go.name);
+
+        if (go == lastTriggerGo)
+        {
+            return;
+        }
+
+        lastTriggerGo = go;
+
+        if (go.tag == "Enemy")
+        {
+            shieldLevel--;
+            Destroy(go);
+        }
+        else
+        {
+            print("Triggered by non-Enemy: " + go.name);
+        }
+    }
+
+    public float shieldLevel
+    {
+        get
+        {
+            return (_shieldLevel);
+        }
+
+        set
+        {
+            _shieldLevel = Mathf.Min(value, 4);
+
+            if (value < 0)
+            {
+                Destroy(this.gameObject);
+
+                Main.S.DelayedRestart(gameRestartDelay);
+            }
+        }
+    }
 }
